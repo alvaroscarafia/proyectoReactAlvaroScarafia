4e880e23aec6ca2c8203bc869c878022fb15421d
Alvaro Scarafia <alvaro_s.d@hotmail.com>
2022-08-22 18:03:24 -0300

    React Proyect

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Bienvenido a mi proyecto de ReactJs.
+        </p>
+        <h2>Alvaro Scarafia</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
